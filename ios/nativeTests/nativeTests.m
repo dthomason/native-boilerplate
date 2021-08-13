712848f9774534dc1c5936a5cb6363eabae96ea6
David Thomason <dthomason@frontporch.com>
2021-08-12 20:54:01 -0700

    renamed

--- ios/nativeTests/nativeTests.m
+++ ios/nativeTests/nativeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reflectimeTests : XCTestCase
+@interface nativeTests : XCTestCase
 
 @end
 
-@implementation reflectimeTests
+@implementation nativeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
