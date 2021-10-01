21eeae04fea708f3c162b328f28e8d48bdf2b181
David <dthomason@frontporch.com>
2021-10-01 01:53:39 -0700

    fix name plist conflict (#2)
    * Change name to base
    
    * too many options

--- ios/baseTests/baseTests.m
+++ ios/baseTests/baseTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface nativeTests : XCTestCase
+@interface baseTests : XCTestCase
 
 @end
 
-@implementation nativeTests
+@implementation baseTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
