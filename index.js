2c1be2fc3017572d5ae2560062fe35b929587441
David Thomason <dthomason@frontporch.com>
2021-10-01 01:49:50 -0700

    Change name to base

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * @format
  */
 
-import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
+import 'react-base-gesture-handler';
+import { AppRegistry } from 'react-base';
 import App from './app/Entrypoint';
 import { name as appName } from './app.json';
-import { enableScreens } from 'react-native-screens';
+import { enableScreens } from 'react-base-screens';
 enableScreens();
 
 AppRegistry.registerComponent(appName, () => App);
